4a9b801f5c781177f6553e5fd8a73d4858ede8a2
harryhai <harryhaivn@gmail.com>
2019-06-06 16:24:30 +0800

    update take picture and display image

--- lib/presentation/camera/CameraScreen.dart
+++ lib/presentation/camera/CameraScreen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CameraScreen extends StatefulWidget {
   List<CameraDescription> cameras;
@@ -19,7 +23,7 @@ class CameraScreenState extends State<CameraScreen> {
   void initState() {
     super.initState();
     controller =
-        new CameraController(widget.cameras[0], ResolutionPreset.medium);
+    new CameraController(widget.cameras[0], ResolutionPreset.medium);
     controller.initialize().then((_) {
       if (!mounted) {
         return;
@@ -36,12 +40,75 @@ class CameraScreenState extends State<CameraScreen> {
 
   @override
   Widget build(BuildContext context) {
-    if (!controller.value.isInitialized) {
-      return new Container(child: Center(child: CircularProgressIndicator()),);
-    }
-    return new AspectRatio(
-      aspectRatio: controller.value.aspectRatio,
-      child: new CameraPreview(controller),
+    return Scaffold(
+      appBar: AppBar(title: Text('Take a picture')),
+      // You must wait until the controller is initialized before displaying the
+      // camera preview. Use a FutureBuilder to display a loading spinner until
+      // the controller has finished initializing
+      body: FutureBuilder<void>(
+        future: controller.initialize(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview
+            return CameraPreview(controller);
+          } else {
+            // Otherwise, display a loading indicator
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        // Provide an onPressed callback
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure the camera is initialized
+            await controller.initialize();
+
+            // Construct the path where the image should be saved using the path
+            // package.
+            final path = join(
+              // In this example, store the picture in the temp directory. Find
+              // the temp directory using the `path_provider` plugin.
+              (await getTemporaryDirectory()).path,
+              '${DateTime.now()}.png',
+            );
+
+            // Attempt to take a picture and log where it's been saved
+            await controller.takePicture(path);
+
+            // If the picture was taken, display it on a new screen
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(imagePath: path),
+              ),
+            );
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+      ),
+    );
+  }
+}
+
+// A Widget that displays the picture taken by the user
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Display the Picture')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image
+      body: Image.file(File(imagePath)),
     );
   }
 }
